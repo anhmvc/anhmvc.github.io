8fb6d192b1116285d3c03c54b24599f9a2aea86e
Anh Mac <anhmvc@gmail.com>
2024-01-29 20:51:58 -0800

    Added projects

--- src/pages/Work/Work.tsx
+++ src/pages/Work/Work.tsx
@@ -4,16 +4,64 @@ import "./work.css";
 import { Card } from "../../components";
 
 export default function Work() {
+  const PROJECTS = [
+    {
+      title: "Royce Hall Simulator",
+      subtitle: "Computer Graphics, 3D Modeling",
+      tech: ["Javascript", "three.js", "Blender"],
+      backgroundColor: "Teal",
+      links: ["github"],
+    },
+    {
+      title: "Twain",
+      subtitle: "Chrome Web Extension, Full Stack Development",
+      tech: ["Javascript", "React.js", "Web Development", "API Development"],
+      backgroundColor: "Teal",
+      links: ["github"],
+    },
+    {
+      title: "Saru Recycling",
+      subtitle: "Web/Mobile Development, UX/UI Design",
+      tech: ["Javascript", "React.js", "React Native", "Firebase", "Figma"],
+      backgroundColor: "Teal",
+      links: ["github", "figma"],
+    },
+    {
+      title: "The MET Explorer",
+      subtitle: "Data Science",
+      tech: ["Python", "Jupyter Notebook", "Tableau"],
+      backgroundColor: "Teal",
+      links: ["github"],
+    },
+    {
+      title: "Code Connects",
+      subtitle: "UX/UI Design",
+      tech: ["Figma"],
+      backgroundColor: "Teal",
+      links: ["figma"],
+    },
+    {
+      title: "FLUX",
+      subtitle: "UX/UI Design",
+      tech: ["Figma"],
+      backgroundColor: "Teal",
+      links: ["figma"],
+    },
+  ];
   return (
     <div className="page-container">
       <h1 className="title">work</h1>
-      <Card
-        title="Royce Hall Simulator"
-        subtitle="Computer Graphics, 3D Modeling"
-        tech={["Javascript", "three.js", "Blender"]}
-        backgroundColor="Purple"
-        links={["github", "figma"]}
-      />
+      {PROJECTS.map((project) => {
+        return (
+          <Card
+            title={project.title}
+            subtitle={project.subtitle}
+            tech={project.tech}
+            backgroundColor={project.backgroundColor}
+            links={project.links}
+          />
+        );
+      })}
     </div>
   );
 }
